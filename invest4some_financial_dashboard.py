c431eff96a7cea8efae3cc3c9da69abcfd5bfb31
André Forte <90764641+andremforte@users.noreply.github.com>
2022-05-30 11:58:10 +0100

    Add files via upload

--- invest4some_financial_dashboard.py
+++ invest4some_financial_dashboard.py
@@ -249,28 +249,28 @@ if options == 'Cryptocurrencies':
 
 			## Calculate the buy & sell signals
 		for x in range(15, len(df)):
-		    
-		    # Calculate "Long Tomorrow" column
-		    if ((df['RSI'][x] <= 40) & (df['RSI'][x-1]>40) ):
-		        df['Long Tomorrow'][x] = True
-		    elif ((df['Long Tomorrow'][x-1] == True) & (df['RSI'][x] <= 70)):
-		        df['Long Tomorrow'][x] = True
-		    else:
-		        df['Long Tomorrow'][x] = False
-		        
-		    # Calculate "Buy Signal" column
-		    if ((df['Long Tomorrow'][x] == True) & (df['Long Tomorrow'][x-1] == False)):
-		        df['Buy Signal'][x] = df['Adj Close'][x]
-		        df['Buy RSI'][x] = df['RSI'][x]
-		        
-		    # Calculate "Sell Signal" column
-		    if ((df['Long Tomorrow'][x] == False) & (df['Long Tomorrow'][x-1] == True)):
-		        df['Sell Signal'][x] = df['Adj Close'][x]
-		        df['Sell RSI'][x] = df['RSI'][x]
+
+			# Calculate "Long Tomorrow" column
+			if ((df['RSI'][x] <= 40) & (df['RSI'][x-1]>40) ):
+			    df['Long Tomorrow'][x] = True
+			elif ((df['Long Tomorrow'][x-1] == True) & (df['RSI'][x] <= 70)):
+			    df['Long Tomorrow'][x] = True
+			else:
+			    df['Long Tomorrow'][x] = False
+			    
+			# Calculate "Buy Signal" column
+			if ((df['Long Tomorrow'][x] == True) & (df['Long Tomorrow'][x-1] == False)):
+			    df['Buy Signal'][x] = df['Adj Close'][x]
+			    df['Buy RSI'][x] = df['RSI'][x]
+			    
+			# Calculate "Sell Signal" column
+			if ((df['Long Tomorrow'][x] == False) & (df['Long Tomorrow'][x-1] == True)):
+			    df['Sell Signal'][x] = df['Adj Close'][x]
+			    df['Sell RSI'][x] = df['RSI'][x]
 
 			## Calculate strategy performance
-		df['Strategy'][15] = df['Adj Close'][15]
 
+		df['Strategy'][15] = df['Adj Close'][15]
 
 		for x in range(16, len(df)):
 		    if df['Long Tomorrow'][x-1] == True:
@@ -278,8 +278,7 @@ if options == 'Cryptocurrencies':
 		    else:
 		        df['Strategy'][x] = df['Strategy'][x-1]
 
-
-		return df
+	    return df
 
 	data = indicators(crypto1)
 
@@ -873,7 +872,6 @@ if options == 'Currencies':
 		df['RSI'][14] = 100 - (100/(1+df['RS'][14]))
 
 
-
 			## Calculate rest of Average Up, Average Down, RS, RSI
 		for x in range(15, len(df)):
 		    df['Average Up'][x] = (df['Average Up'][x-1]*13+df['Up Move'][x])/14
@@ -900,7 +898,7 @@ if options == 'Currencies':
 		    elif ((df['Long Tomorrow'][x-1] == True) & (df['RSI'][x] <= 70)):
 		        df['Long Tomorrow'][x] = True
 		    else:
-		        df['Long Tomorrow'][x] = False
+		    	df['Long Tomorrow'][x] = False
 		        
 		    # Calculate "Buy Signal" column
 		    if ((df['Long Tomorrow'][x] == True) & (df['Long Tomorrow'][x-1] == False)):
